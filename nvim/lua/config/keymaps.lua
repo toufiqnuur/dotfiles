3c207a5a88639a6abc114e9751c0d80a5e35b2ec
toufiqnuur <taufik2017@gmail.com>
2024-08-11 19:35:16 +0700

    init: new config

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Normal Mode" })
+vim.keymap.set("n", ";", ":", { desc = "Colon Alias" })
---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,4 @@
+return {
+  { "rcarriga/nvim-notify", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+}
